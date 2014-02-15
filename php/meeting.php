67ebcd8bc500f9c6beab7b7ef0ac720c655284d9
pink <pink@cottontracks.com>
2014-02-15 19:15:37 +0100

    Meeting: set user and comment form

---
+++ php/meeting.php
@@ -0,0 +1,47 @@
+<?php
+  require_once('connect.php');
+  require_once('clean.php');
+
+  $hash = clean($_GET['h']);
+
+  $select_hash_qry = "SELECT * FROM link WHERE hash='$hash'";
+  $select_hash_result = @mysql_query($select_hash_qry);
+  if ($select_hash_result) {
+    $res = mysql_fetch_assoc($select_hash_result);
+
+    $user_id = $res['user_id'];
+    $meeting_id = $res['meeting_id'];
+
+  }
+
+?>
+
+
+<html>
+<head>
+	<title>mtng</title>
+</head>
+
+<body>
+  <form action='comment.php' method="post">
+    <?php
+      if ($user_id == 0) {
+        echo
+          '<input type="text" name="first_name" id="first_name" placeholder="first name"></input>',
+          '<input type="text" name="family_name" id="family_name" placeholder="family name"></input>',
+          '<input type="text" name="phone" id="phone" placeholder="phone"></input>',
+          '<input type="text" name="email" id="email" placeholder="email"></input>';
+      } else {
+        echo '<input name="author_id" type="hidden" value="',$res['user_id'],'">';
+      }
+    ?>
+    <textarea name="comment" id="comment" placeholder="Comment"></textarea>
+    <?php
+      echo '<input name="hash" type="hidden" value="',$hash,'">';
+      echo '<input name="meeting_id" type="hidden" value="',$meeting_id,'">';
+    ?>
+    <input type='submit' id="submit" value="Send">
+	</form>
+
+</body>
+</html>
