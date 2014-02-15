e806fb2fa5e645bf69089c63fb7f79ae76e80d77
pink <pink@cottontracks.com>
2014-02-15 19:02:27 +0100

    Tools: connect db

--- php/link.php
+++ php/link.php
@@ -1,18 +1,5 @@
 <?php
-	require_once('config.php');
-
-	//Connect to mysql server
-	$connection = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-	if(!$connection) {
-		die('Failed to connect to server: ' . mysql_error());
-	}
-
-	//Select database
-	$db = mysql_select_db(DB_DATABASE, $connection);
-	if(!$db) {
-		die("Unable to select database");
-	}
-
+	require_once('connect.php');
   require_once('clean.php');
 
   $first_name = clean($_POST['first_name']);
