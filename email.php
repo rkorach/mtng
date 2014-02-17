438280483de5c9f5162899cf5b0fa7225e46204f
pink <pink@cottontracks.com>
2014-02-17 10:34:54 +0100

    Email: remove bcc

--- email.php
+++ email.php
@@ -1,7 +1,6 @@
 <?php
 function sendEmail($to,$message){
-	$hearders="From: noreply@mtng.eu\n
-						 Bcc: contact@cottontracks.com\n"; //Bcc contact@ for the test	
+	$hearders="From: noreply@mtng.eu";
 	$subject="Your question has been answered on mtng.eu";
 	mail($to,$subject,$message,$headers);
 }
