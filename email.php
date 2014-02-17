17b1eab0eac4ce8d60f4aba0c5dbc9db14f14cfb
pink <pink@cottontracks.com>
2014-02-17 17:13:39 +0100

    Refactor: fix sendEmail

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 function sendEmail($to,$message){
-	$hearders="From: noreply@mtng.eu";
+	$headers="From: noreply@mtng.eu";
 	$subject="Your question has been answered on mtng.eu";
 	mail($to,$subject,$message,$headers);
 }
