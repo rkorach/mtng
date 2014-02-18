96dbe837fb710b596259c7aa7d1ddaf68f5f53a0
Julihan <julien.deveaux@gmail.com>
2014-02-18 18:34:31 +0100

    fix sendSMS function with the correct OVH API

--- sms.php
+++ sms.php
@@ -1,33 +1,19 @@
 <?php
 function sendSMS($to,$message){
 	//Seesion OVH
-	$nic="mtng";
+	$user="mtng";
 	$pass="a2OsMijk";
 	$sms_compte="sms-dj219716-1";
 	
 	//Our OVH phone number
 	$from="COTTONTRACK";
 	
-	try
-	{
-	$soap = new SoapClient("https://www.ovh.com/soapi/soapi-re-1.8.wsdl");
-	// $session = $soap->login("$nic", "$pass","fr", false);
-	
-	//echo "login successfull\n"; //Test connexion
-	
-	$result = $soap->telephonySmsSend("$nic", "$pass", "$sms_compte", "$from", "$to", "$message");
-	
-	//echo "telephonySmsSend successfull\n"; //Test send
-	
-	print_r($result);
-	
-	$soap->logout($session);
-	
-	//echo "logout successfull\n"; //Test log out
+	try{
+	$soap = new SoapClient("https://www.ovh.com/soapi/soapi-re-1.29.wsdl");
+	$result = $soap->telephonySmsUserSend("$user", "$pass", "$sms_compte", "$from", "$to", "$message");
 	}
 	
-	catch(SoapFault $fault)
-	{
+	catch(SoapFault $fault){
 	echo $fault;
 	}
 }
